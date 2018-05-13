01a9421ffca163f66a67843c60b9d15b8618d0d0
Arnaud Thimel <thimel@codelutin.com>
2018-05-13 16:20:54 +0200

    doc

--- rest/src/main/java/org/zoumbox/mountyMonsters/rest/v1/ParserService.java
+++ rest/src/main/java/org/zoumbox/mountyMonsters/rest/v1/ParserService.java
@@ -16,6 +16,13 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 public class ParserService {
 
+    /**
+     * Essaye de calculer un maximum d'informations sur un monstre à partir de son nom. Exemple :
+     * <pre>Maîtresse Ame-en-peine [Naissante]</pre>
+     *
+     * @param raw le nom brut
+     * @return Les informations déduites à propos du monstre
+     */
     @GET
     @Path("/fromName")
     public ImmutableMonster fromName(@QueryParam("raw") String raw) {
@@ -24,9 +31,19 @@ public class ParserService {
         return result;
     }
 
+    /**
+     * Essaye de calculer un maximum d'informations sur un monstre à partir de la ligne extraite de Sp_Vue2.php).
+     * Exemple :
+     * <pre>5864923;Maîtresse Ame-en-peine [Naissante];-74;-40;-78</pre>
+     *
+     * @param row La ligne brute provenant de Sp_Vue2.php
+     * @return Les informations déduites à propos du monstre
+     * @see #fromName(String)
+     */
     @GET
     @Path("/fromSpVue2Row")
     public ImmutableMonster fromSpVue2Row(@QueryParam("row") String row) {
+
         List<String> strings = Splitter.on(";").splitToList(row);
         String name = strings.get(1);
         ImmutableMonster result = fromName(name);
