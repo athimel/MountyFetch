c9c3bd545f036fc5b40952ca274defe565330480
Arnaud Thimel <thimel@codelutin.com>
2018-06-20 10:26:16 +0200

    Adapter sur la sérialisation des Range

--- rest/src/main/java/org/zoumbox/mountyFetch/rest/Main.java
+++ rest/src/main/java/org/zoumbox/mountyFetch/rest/Main.java
@@ -1,6 +1,8 @@
 package org.zoumbox.mountyFetch.rest;
 
+import com.google.common.collect.Range;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -13,17 +15,24 @@ import java.net.URI;
 
 /**
  * Main class.
- *
  */
 public class Main {
 
     public static final String BASE_URI = "http://0.0.0.0:8080/mountyFetch/";
 
+    protected static Gson buildGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(Range.class, new ToStringSerializer());
+        Gson gson = gsonBuilder.create();
+        return gson;
+    }
+
     public static HttpServer startServer() {
+
         final ResourceConfig rc = new ResourceConfig().packages("org.zoumbox.mountyFetch.rest");
         GsonMessageBodyProvider provider = new GsonMessageBodyProvider(
                 new GsonProviderOptionsBuilder()
-                        .gson(new Gson()) // build custom Gson instance using GsonBuilder methods
+                        .gson(buildGson()) // build custom Gson instance using GsonBuilder methods
                         .addMediaTypes(MediaType.APPLICATION_JSON_TYPE) // specify custom media types
                         .addMediaTypes(MediaType.APPLICATION_JSON_TYPE.withCharset("utf-8"))
                         .build()) {
